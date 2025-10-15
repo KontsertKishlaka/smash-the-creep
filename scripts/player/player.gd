ce5877da121530968f29b3c87305fc3daf814c99
nineteentearz <kakao10@yandex.ru>
2025-10-15 23:19:26 +0300

    feat: add player jump | refactor: player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,7 +1,17 @@
 extends CharacterBody3D
+class_name Player
 
 @export var mouse_sensitivity: float = 0.002
 @export var max_degree: float = 45
+@export var speed: float = 2
+@export var jump_velocity: float = 7
+@export var health_points: int = 100
+
+@onready var camera: Camera3D = $Camera
+
+const GRAVITY = 9.8
+
+var y_velocity: float = 0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -10,30 +20,26 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 
-		var camera = get_node("Camera3D")
-
 		if camera:
 			var current_rotation_x = camera.rotation_degrees.x
 			var target_rotation_x = clamp(current_rotation_x - event.relative.y * mouse_sensitivity * 100, -max_degree, max_degree)
 			camera.rotation_degrees.x = target_rotation_x
 
-const SPEED = 2
-const JUMP_VELOCITY = 50
-const GRAVITY = 9.8
 
-var y_velocity = 0
 
 func _physics_process(delta: float):
 	
 	print("Is on floor: ", is_on_floor())
 	
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		y_velocity = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_pressed("jump"):
+			y_velocity = jump_velocity
 	
 	if not is_on_floor():
 		y_velocity -= GRAVITY * delta
 	else:
-		y_velocity = 0
+		if y_velocity < 0:
+			y_velocity = 0.0
 
 	var input_dir = Vector3.ZERO
 
@@ -52,8 +58,8 @@ func _physics_process(delta: float):
 
    
 	var direction = (transform.basis * input_dir).normalized()
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
 	velocity.y = y_velocity  
 
 	move_and_slide()
