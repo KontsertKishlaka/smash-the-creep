d543c6050c0602abb1731823bf4f6b2a4f8874df
Kirill <bukarev.k11@gmail.com>
2025-10-16 22:48:03 +0300

    feat(enemy): add look duration

--- scripts/enemy/states/chase_state.gd
+++ scripts/enemy/states/chase_state.gd
@@ -1,5 +1,11 @@
 extends EnemyState
 
+var look_timer: float = 0.0
+var look_duration: float = 1.5
+
+func enter():
+	look_timer = 0.0
+
 func physics_update(delta):
 	if not slime.player or not slime.data:
 		return
@@ -10,6 +16,18 @@ func physics_update(delta):
 	var distance = to_player.length()
 	var dir = to_player.normalized()
 
+	# --- Если игрок ушёл далеко ---
+	if distance > slime.data.detection_range * 1.5:
+		if look_timer <= 0.0:
+			look_timer = look_duration
+		else:
+			look_timer -= delta
+			
+			slime._rotate_toward(dir, delta)
+			if look_timer <= 0.0:
+				state_machine.change_state("PatrolState")
+		return 
+
 	# --- Движение к игроку ---
 	slime.velocity.x = dir.x * slime.data.speed
 	slime.velocity.z = dir.z * slime.data.speed
@@ -19,7 +37,3 @@ func physics_update(delta):
 	if slime.is_on_floor() and slime.jump_timer <= 0.0 and distance < slime.data.jump_distance:
 		slime.velocity.y = slime.data.jump_velocity
 		slime.jump_timer = slime.data.jump_cooldown
-
-	# --- Если игрок ушёл далеко, возвращаемся к патрулю ---
-	if distance > slime.data.detection_range * 1.5:
-		state_machine.change_state("PatrolState")
