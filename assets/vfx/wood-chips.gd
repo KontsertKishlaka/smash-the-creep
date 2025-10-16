94f90e02528974148aa4ba510e6296cb3d7d6c7c
MindlessMuse666 <mindlessmuse.666@gmail.com>
2025-10-17 02:10:24 +0300

    fix(vfx): view wood chips

--- assets/vfx/wood-chips.gd
+++ assets/vfx/wood-chips.gd
@@ -1,5 +1,8 @@
 extends Node3D
+class_name WoodChips
+
+@onready var particles: GPUParticles3D = $Particles
 
 func _ready():
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(particles.lifetime).timeout
 	queue_free()
