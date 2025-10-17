e54443818d40164940156640192fdd388192912e
Kirill <bukarev.k11@gmail.com>
2025-10-17 21:59:15 +0300

    feat: reload proj
    Co-Authored-By: Vladislav <153318140+MindlessMuse666@users.noreply.github.com>

--- smash-the-creep/scripts/enemy/states/death-state.gd
+++
@@ -1,9 +0,0 @@
-extends EnemyState
-class_name DeathState
-
-@export var state_enum: int = EnemyStatesEnum.State.DeathState
-
-func enter():
-	print("Slime умер!")
-	# Здесь можно проиграть анимацию смерти :p
-	queue_free()
